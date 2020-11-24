16e67d2f64ec2f7e28111e4048bf4aaa3efef725
Pranav50 <pranavpwr815@gmail.com>
2020-11-25 00:54:31 +0530

    make it better

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    MONGO_URI: "mongodb+srv://Pranav100:N7YK6bYa6Sxuo8RU@ecommerce.y449f.mongodb.net/ecommerce?retryWrites=true&w=majority",
+    JWT_SECRET: 'KJHJSGHDTYUISOPMSG',
+    BRAINTREE_MERCHANT_ID='gv3ppfvrcc9mnpwh',
+    BRAINTREE_PUBLIC_KEY='fkrjnkk73xt3695g',
+    BRAINTREE_PRIVATE_KEY='62ce7bae9df3ee196cd1ca0dde7f3fc1',
+}
